2af441b3f9f63c6924c0a18f758c6fe4bc6c15fb
Lauren Albert <pleaseonthekeys@gmail.com>
2019-11-21 18:06:16 -0500

    debugging for heroku

--- db/index.js
+++ db/index.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "us-cdbr-iron-east-05.cleardb.net"
+  host: "us-cdbr-iron-east-05.cleardb.net",
   user: "bb9287dba574c6",
-  password: "c37795ee"
+  password: "c37795ee",
   database: "heroku_40b468769343705"
 });
 
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const parser = require("body-parser");
-const port = 8000;
+// const port = 8000;
 
 const SongRouter = require("./routes.js");
 
@@ -10,4 +10,9 @@ app.use(express.static("dist"));
 app.use(SongRouter);
 
 app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
