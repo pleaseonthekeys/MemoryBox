3b298eb40987f66b70ee679d525f71aa79c2da9a
Lauren Albert <pleaseonthekeys@gmail.com>
2019-11-21 18:12:28 -0500

    still debugging for heroku

--- db/index.js
+++ db/index.js
@@ -1,16 +1,28 @@
+// const mysql = require("mysql");
+
+// const connection = mysql.createConnection({
+//   host: "us-cdbr-iron-east-05.cleardb.net",
+//   user: "bb9287dba574c6",
+//   password: "c37795ee",
+//   database: "heroku_40b468769343705"
+// });
+
+// connection.connect(err => {
+//   err
+//     ? console.log("unable to connect to db", err)
+//     : console.log("connected to db");
+// });
+
+// module.exports = connection;
+
 const mysql = require("mysql");
+const dbConfig = require("./db.config.js");
 
-const connection = mysql.createConnection({
-  host: "us-cdbr-iron-east-05.cleardb.net",
-  user: "bb9287dba574c6",
-  password: "c37795ee",
-  database: "heroku_40b468769343705"
-});
-
-connection.connect(err => {
-  err
-    ? console.log("unable to connect to db", err)
-    : console.log("connected to db");
+var connection = mysql.createPool({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB
 });
 
 module.exports = connection;
