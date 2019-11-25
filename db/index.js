415f838b306779adbb4cd3f3ada7f7c004dee219
Lauren Albert <pleaseonthekeys@gmail.com>
2019-11-25 16:53:36 -0500

    started db connection

--- db/index.js
+++ db/index.js
@@ -16,11 +16,22 @@
 const mysql = require("mysql");
 const dbConfig = require("./db.config");
 
-var connection = mysql.createPool({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
-});
+// var connection = mysql.createPool({
+//   host: dbConfig.HOST,
+//   user: dbConfig.USER,
+//   password: dbConfig.PASSWORD,
+//   database: dbConfig.DB
+// });
 
+var connection = mysql.createConnection(
+  "mysql://bb9287dba574c6:c37795ee@us-cdbr-iron-east-05.cleardb.net/heroku_40b468769343705?reconnect=true"
+);
+
+connection.connect(err => {
+  if (err) {
+    console.log("unable to connect to mysql", err);
+  } else {
+    console.log("connected to mysql DB!");
+  }
+});
 module.exports = connection;
